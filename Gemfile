08110eab40b31ad352c6d25f56352268d068f054
nguyenvu <nguyenvu1892@gmail.com>
2018-03-09 21:14:09 +0700

    edit_subject

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem "sqlite3"
 gem "therubyracer", platforms: :ruby
 gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
-gem "will_paginate", ">= 3.0"
+gem "bootstrap-will_paginate"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,5 +4,9 @@ class Subject < ApplicationRecord
   has_many :user, through: :user_subjects
   has_many :course_subject
 
-  accepts_nested_attributes_for :tasks, reject_if: lambda {|a| a[:tasks_id].blank?}, allow_destroy: true
+  accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
+
+  validates :subject_name, presence: true
+
+  scope :only_id_name_field, -> {select :id, :subject_name}
 end
